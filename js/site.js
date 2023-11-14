78f91238b10ab51f1c3ceed705a36986f9cce363
mharofuentes <90435653+mharofuentes@users.noreply.github.com>
2023-11-14 04:28:47 -0600

    Update site.js
    fixed according to syntax Validator

--- js/site.js
+++ js/site.js
@@ -1,13 +1,9 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-}
+
 function validateForm() {
-  let x = document.forms["Form"]["input-email].value;
+  let x = document.forms["Form"]["input-email"].value;
   if (x == "") {
     alert("E-mail field must be filled out before submitting!");
     return false;
   }
-}
-});
+
+};
