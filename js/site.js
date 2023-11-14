623d3fa41de6b8f31f18fe1af3c3ac0812c2e3e0
mharofuentes <90435653+mharofuentes@users.noreply.github.com>
2023-11-14 03:43:57 -0600

    Update site.js
    added javascript validation

--- js/site.js
+++ js/site.js
@@ -2,4 +2,12 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+}
+function validateForm() {
+  let x = document.forms["Form"]["input-email].value;
+  if (x == "") {
+    alert("E-mail field must be filled out before submitting!");
+    return false;
+  }
+}
 });
